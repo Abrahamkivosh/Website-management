648a420807012470503419d01d6d56fdd2551c0b
root <abrahamkivosh@gmail.com>
2018-12-21 09:12:28 +0300

    I added policies.php page in my first branch but i have not merged it with master branch

---
+++ policies.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>Page Title</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" type="text/css" media="screen" href="style/style.css" />
+</head>
+
+<body>
+    <div class="container" id="container">
+        <p class="title">System management system</p>
+        <div class="header">
+            POLICIES OF OUR COMPANY
+        </div>
+        <div class="read">PLEASE READ THEM CAREFULLY </div>
+
+    </div>
+
+</body>
+
+</html>
